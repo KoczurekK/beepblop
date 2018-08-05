3fed60a77135c86ab85bc65ae0f323ddfab032c0
Kamil Koczurek <koczurekk@gmail.com>
2018-08-05 23:39:42 +0200

    Background draws all 3 layers, but they move at the same rate

--- source/background.d
+++ source/background.d
@@ -16,28 +16,37 @@ static this() {
 }
 
 class Background: Drawable {
-  private BigSprite layer1;
+  private BigSprite[3] layers;
   private Vector2f offset1 = Vector2f(0, 0);
 
   this() {
-    layer1 = new BigSprite;
-    layer1.textures = [
-      manager["bg1a"], manager["bg1b"],
-      manager["bg1c"], manager["bg1d"]
-    ];
-    layer1.position = Vector2f(0, 0);
+    foreach(n; 0..3) {
+      layers[n] = new BigSprite;
+    }
+
+    foreach(n; 0..3) {
+      immutable nstr = (n + 1).to!string;
+      layers[n].textures = [
+        manager["bg" ~ nstr ~ "a"], manager["bg" ~ nstr ~ "b"],
+        manager["bg" ~ nstr ~ "c"], manager["bg" ~ nstr ~ "d"]
+      ];
+    }
+
+    foreach(l; layers) {
+      l.position = Vector2f(0, 0);
+    }
   }
 
   void move(in Vector2f shift) {
     offset1 = Vector2f(
-      fmod(offset1.x + shift.x, layer1.size.x),
-      fmod(offset1.y + shift.y, layer1.size.y)
+      fmod(offset1.x + shift.x, layers[0].size.x),
+      fmod(offset1.y + shift.y, layers[0].size.y)
     );
-    while(offset1.x < -layer1.size.x) {
-      offset1.x += layer1.size.x;
+    while(offset1.x < -layers[0].size.x) {
+      offset1.x += layers[0].size.x;
     }
-    while(offset1.y < -layer1.size.y) {
-      offset1.y += layer1.size.y;
+    while(offset1.y < -layers[0].size.y) {
+      offset1.y += layers[0].size.y;
     }
   }
 
@@ -45,15 +54,20 @@ class Background: Drawable {
     immutable origin = rt.mapPixelToCoords(Vector2i(0, 0));
     immutable rt_sz = rt.mapPixelToCoords(cast(Vector2i) rt.getSize) - origin;
 
-    auto l1 = layer1.dup;
+    auto copy = [
+      layers[0].dup,
+      layers[1].dup,
+      layers[2].dup
+    ];
+    foreach(layer; copy) {
+      int xreps = cast(int) (cast(real) rt_sz.x / layer.size.x).ceil + 1;
+      int yreps = cast(int) (cast(real) rt_sz.y / layer.size.y).ceil + 1;
 
-    int xreps = cast(int) (cast(real) rt_sz.x / l1.size.x).ceil + 1;
-    int yreps = cast(int) (cast(real) rt_sz.y / l1.size.y).ceil + 1;
-
-    foreach(x; -1 .. xreps)
-    foreach(y; -1 .. yreps) {
-      l1.position = origin + offset1 + Vector2f(l1.size.x * x, l1.size.y * y);
-      rt.draw(l1, rs);
+      foreach(x; -1 .. xreps)
+      foreach(y; -1 .. yreps) {
+        layer.position = origin + offset1 + Vector2f(layer.size.x * x, layer.size.y * y);
+        rt.draw(layer, rs);
+      }
     }
   }
 }
