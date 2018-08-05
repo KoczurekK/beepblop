2286181b5855c581bbdaa997387e76de2700fa6e
Kamil Koczurek <koczurekk@gmail.com>
2018-08-05 23:45:58 +0200

    Enabled v-sync

--- source/app.d
+++ source/app.d
@@ -8,6 +8,8 @@ void main() {
   manager.load();
 
 	auto win = new RenderWindow(VideoMode.getDesktopMode, "game", Window.Style.Fullscreen);
+  win.setVerticalSyncEnabled(true);
+
   auto game = new Game(win);
 
   game.run;
