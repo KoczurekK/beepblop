6c3ab13069da69e15eb3357614f8c698c6baa09f
Kamil Koczurek <koczurekk@gmail.com>
2018-08-16 20:47:42 +0200

    Implemented a global sound pool

---
+++ source/sounds.d
@@ -0,0 +1,28 @@
+module sounds;
+
+import resourcemanager;
+import std.algorithm;
+import dsfml.audio;
+
+class SoundPool {
+  Sound[] sounds;
+
+  private this() {}
+
+  void play(string name) {
+    sounds ~= new Sound(manager.audio(name));
+    sounds[$ - 1].play;
+  }
+
+  void cleanup() {
+    sounds = sounds.remove!(s => s.status == Sound.Status.Stopped);
+  }
+
+  static SoundPool sound_pool;
+
+  static this() {
+    sound_pool = new SoundPool;
+  }
+}
+
+alias sound_pool = SoundPool.sound_pool;
