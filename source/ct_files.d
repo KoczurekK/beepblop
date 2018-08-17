9852c9bfec809e4333fd9b2087202f69f68681b4
Kamil Koczurek <koczurekk@gmail.com>
2018-08-17 17:15:38 +0200

    CTFile → CTAsset

--- source/ct_files.d
+++ source/ct_files.d
@@ -3,9 +3,9 @@ module ct_files;
 import std.file;
 import std.json;
 
-struct CTFile {
-  static CTFile make(string _path)() {
-    CTFile res;
+struct CTAsset {
+  static CTAsset make(string _path)() {
+    CTAsset res;
 
     res.path = _path;
     res.text = import(_path);
@@ -17,16 +17,16 @@ struct CTFile {
   string text;
 }
 
-immutable CTFile[] static_assets = () {
+immutable CTAsset[] static_assets = () {
   enum jval = parseJSON(import("assets/assets.json"));
-  CTFile[] ctfs;
+  CTAsset[] ctfs;
 
   static foreach(type; ["textures", "audio"])
   static foreach(n; 0 .. jval[type].array.length) {
     // Additional bracket level to avoid name collision
     {
       enum str = jval[type][n]["path"].str;
-      ctfs ~= CTFile.make!str;
+      ctfs ~= CTAsset.make!str;
     }
   }
 
